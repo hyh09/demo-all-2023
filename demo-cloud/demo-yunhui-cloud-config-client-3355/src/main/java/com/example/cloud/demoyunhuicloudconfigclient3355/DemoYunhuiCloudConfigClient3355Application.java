2e928f83f56833dc3526e72a710239fe5ca86a8e
wb04 <455663056@qq.com>
2023-02-28 22:29:40 +0800

    ###搭建springCloud config的服务端配置中心 客户端-非注册中心情况下的使用

---
+++ demo-cloud/demo-yunhui-cloud-config-client-3355/src/main/java/com/example/cloud/demoyunhuicloudconfigclient3355/DemoYunhuiCloudConfigClient3355Application.java
@@ -0,0 +1,11 @@
+package com.example.cloud.demoyunhuicloudconfigclient3355;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoYunhuiCloudConfigClient3355Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoYunhuiCloudConfigClient3355Application.class, args);
+    }
+}
