bf087daca698b62ef2a39b4d9acc49b0d3ff4f34
wb04 <455663056@qq.com>
2023-02-28 22:51:35 +0800

    ###搭建springCloud config的服务端配置中心 手动刷新spring-cloud-config的配置

--- demo-cloud/demo-yunhui-cloud-config-client-3355/src/main/java/com/example/cloud/demoyunhuicloudconfigclient3355/controller/ConfigClientController.java
+++ demo-cloud/demo-yunhui-cloud-config-client-3355/src/main/java/com/example/cloud/demoyunhuicloudconfigclient3355/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.example.cloud.demoyunhuicloudconfigclient3355.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 业务中文描述:
  * Copyright (c) 2023,All Rights Reserved.
  */
+@RefreshScope
 @RestController
 public class ConfigClientController {
     @Value("${server.port}")
