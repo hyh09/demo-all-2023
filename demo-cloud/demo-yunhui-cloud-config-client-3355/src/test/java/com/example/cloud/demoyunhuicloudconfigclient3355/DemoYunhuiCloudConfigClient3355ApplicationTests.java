4ca4f4bc7406dd3401ac75c9ec400898e8c38dc1
wb04 <455663056@qq.com>
2023-02-28 22:30:02 +0800

    ###搭建springCloud config的服务端配置中心 客户端-非注册中心情况下的使用

---
+++ demo-cloud/demo-yunhui-cloud-config-client-3355/src/test/java/com/example/cloud/demoyunhuicloudconfigclient3355/DemoYunhuiCloudConfigClient3355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.demoyunhuicloudconfigclient3355;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoYunhuiCloudConfigClient3355ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
