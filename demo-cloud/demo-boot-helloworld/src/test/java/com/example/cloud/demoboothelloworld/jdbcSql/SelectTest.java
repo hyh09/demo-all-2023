fa2608ef288810fc94d55d6b7b15036929425736
wb_04 <3510789527@qq.com>
2023-03-02 15:23:59 +0800

    ###提交本地的代码

--- demo-cloud/demo-boot-helloworld/src/test/java/com/example/cloud/demoboothelloworld/jdbcSql/SelectTest.java
+++ demo-cloud/demo-boot-helloworld/src/test/java/com/example/cloud/demoboothelloworld/jdbcSql/SelectTest.java
@@ -4,11 +4,10 @@ package com.example.cloud.demoboothelloworld.jdbcSql;
 import com.example.cloud.demoboothelloworld.jdbcSql.annotation.AssembleSql;
 import com.example.cloud.demoboothelloworld.jdbcSql.entity.UserEntity;
 import org.junit.Test;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
 /**
  * @Project Name: demo-all
@@ -57,5 +56,86 @@ public class SelectTest {
 
     }
 
+    @Test
+    public void TestCompare() {
+        Map<String, Object> kafkaDataMap = new HashMap<>() {{
+//            put("name", "100.00");
+            //  put("name", "100.00");
+            put("name1", "100.00");
+            //  put("name2", UUID.fromString("fee930f3-7dce-4bfb-ae22-39edcce5e41f"));
+            put("name3", "1");
+            put("Integer", "10.00");
+            put("bool", "false");
+
+        }};
+        //以目标库的为标准
+        Map<String, Object> databaseDataMap = new HashMap<>() {{
+            //    put("name", new BigDecimal("100"));
+            put("name1", "100.00");
+            put("name3", 1L);
+            put("Integer", 10);
+            put("bool", false);
+
+
+        }};
+        Boolean flg = characterFormtting(kafkaDataMap, databaseDataMap);
+        System.out.println(flg);
+
+    }
+
+
+    private Boolean characterFormtting(Map<String, Object> kafkaDataMap, Map<String, Object> databaseDataMap) {
+        if (kafkaDataMap == null && databaseDataMap != null) {
+            return false;
+        }
+        if (kafkaDataMap != null && databaseDataMap == null) {
+            return false;
+        }
+        //如果key的长度不相等也是不一样
+        if (kafkaDataMap.entrySet().size() != databaseDataMap.entrySet().size()) {
+            return false;
+        }
+        List<String> list = new ArrayList<>();
+        kafkaDataMap.forEach((k1, v1) -> {
+            Object targetValue = databaseDataMap.get(k1);
+            if (targetValue == null && v1 != null) {
+                list.add(k1);
+                return;
+            }
+            if (targetValue != null && v1 == null) {
+                list.add(k1);
+                return;
+            }
+            if (targetValue != null && v1 != null) {
+                if (!targetValue.getClass().isAssignableFrom(v1.getClass())) {
+//                    if (targetValue.getClass().isAssignableFrom(Number.class)) {
+                    if (targetValue instanceof Number) {
+                        Boolean judgeFlg = BigDecimalUtil.INSTANCE.equalOrNot(targetValue, v1);
+                        if (!judgeFlg) {
+                            list.add(k1);
+                            return;
+                        }
+                    } else {
+                        if (!targetValue.toString().equals(v1.toString())) {
+                            list.add(k1);
+                            return;
+                        }
+                    }
+                }
+            } else {
+                if (!targetValue.toString().equals(v1.toString())) {
+                    list.add(k1);
+                    return;
+                }
+            }
+        });
+        if (CollectionUtils.isEmpty(list)) {
+            return true;
+        }
+        System.out.println("list" + list);
+        return false;
+
+
+    }
 
 }
